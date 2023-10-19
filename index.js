a29fa98399681b8aa7efcb63e00d92bcb8769793
RiaJul Pro <riajulpro@outlook.com>
2023-10-19 15:55:45 +0600

    Enable deleting option, D of CRUD

--- index.js
+++ index.js
@@ -73,12 +73,12 @@ async function run() {
       res.send(result);
     });
 
-    // // Deleting specific data from database
-    // app.delete("/products/:id", async (req, res) => {
-    //   const query = { _id: new ObjectId(req.params.id) };
-    //   const result = await moviesCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    // Deleting specific data from database
+    app.delete("/products/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await moviesCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
